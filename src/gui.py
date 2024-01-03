22c4367cb7419140050c0c42bfd86100f8f6c9ca
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2024-01-03 16:15:33 +0100

    New fish design

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,6 @@
 import dearpygui.dearpygui as dpg
 from simulation_parameters import *
+import numpy as np
 
 class GUI:
     def __init__(self):
@@ -8,10 +9,17 @@ class GUI:
 
         # create gui elements
         self.create_gui()
+
+        # set background color
+        dpg.set_viewport_clear_color(color=ColorPalette.background)
         
         # add boids to the canvas
-        self.boids = self.add_boids()
+        self.tails = self.add_tails()
         self.dirs = self.add_dirs()
+        self.dirs_2 = self.add_dirs_2()
+
+        self.particles = self.add_boids()
+        # self.boids = self.add_boids()
 
     def create_gui(self):
         dpg.create_context()
@@ -23,7 +31,6 @@ class GUI:
             height=self.pos2pixels(SP.aquarium_size[1]),
             resizable=False)
         dpg.setup_dearpygui()
-
         
         self.canvas = dpg.add_viewport_drawlist(tag="Canvas", front=False)
 
@@ -34,10 +41,10 @@ class GUI:
                         pos=[720, 740]):
             with dpg.group(horizontal=True):
                 dpg.add_text("Frame rate")
-                dpg.add_text("0", tag="FPS", color=[0, 255, 0, 255])
+                dpg.add_text("0", tag="FPS", color=ColorPalette.fish)
             with dpg.group(horizontal=True):
                 dpg.add_text("Phase")
-                dpg.add_text("0", tag="phase", color=[0, 255, 0, 255])
+                dpg.add_text("0", tag="phase", color=ColorPalette.fish)
             dpg.add_text("Simulation parameters")
             dpg.add_slider_float(
                 label="fish_radius",
@@ -109,15 +116,34 @@ class GUI:
         return params
     
     def update_boids(self, res):
+
         pos = self.pos2pixels(res.pos)
         dir = self.pos2pixels(res.dir*2* dpg.get_value("fish_radius"))
 
+        # new_particles = self.add_boids()
+
         # update gui
         for i in range(SP.num_fish):
-            dpg.configure_item(item=self.boids[i], center=[pos[i, 0], pos[i, 1]], radius=self.pos2pixels(dpg.get_value("fish_radius")))
+            # dpg.configure_item(item=self.boids[i], center=[pos[i, 0], pos[i, 1]], radius=self.pos2pixels(dpg.get_value("fish_radius")))
             dpg.configure_item(item=self.dirs[i],
                                 p2=[pos[i, 0], pos[i, 1]],
                                 p1=[pos[i, 0]-0.5*dir[i, 0], pos[i, 1]-0.5*dir[i, 1]], thickness=self.pos2pixels(dpg.get_value("fish_radius")))
+            dpg.configure_item(item=self.dirs_2[i],
+                                p2=[pos[i, 0], pos[i, 1]],
+                                p1=[pos[i, 0]+0.5*dir[i, 0], pos[i, 1]+0.5*dir[i, 1]], thickness=self.pos2pixels(dpg.get_value("fish_radius")))
+            dpg.configure_item(item=self.tails[i],
+                                p2=[(pos[i, 0]-0.71*dir[i,0]), (pos[i, 1]-0.71*dir[i,1])],
+                                p1=[pos[i, 0]-0.7*dir[i, 0], pos[i, 1]-0.7*dir[i, 1]], thickness=0.5*self.pos2pixels(dpg.get_value("fish_radius")))
+            
+        #     new_particles[i] = dpg.draw_circle(
+        #         center=[pos[i, 0], pos[i, 1]],
+        #         radius=0.1*self.pos2pixels(dpg.get_value("fish_radius")),
+        #         color=ColorPalette.fish,
+        #         fill=ColorPalette.fish,
+        #         parent="Canvas",
+        #     )
+
+        # self.particles += new_particles
 
     def update_frameRate(self, deltaTime):
         dpg.set_value("FPS", str(int(1/deltaTime)))
@@ -131,8 +157,8 @@ class GUI:
             boids.append(dpg.draw_circle(
                 center=[0, 0],
                 radius=self.pos2pixels(dpg.get_value("fish_radius")),
-                color=[0, 255, 0, 255],
-                fill=[0, 255, 0, 255],
+                color=ColorPalette.fish,
+                fill=ColorPalette.fish,
                 parent="Canvas",
             ))
         return boids
@@ -142,9 +168,33 @@ class GUI:
         for i in range(SP.num_fish):
             dirs.append(dpg.draw_arrow(
                 p1=[0, 0],
+                p2=[100, 100],
+                thickness=self.pos2pixels(dpg.get_value("fish_radius")),
+                color=ColorPalette.fish,
+                parent="Canvas",
+            ))
+        return dirs
+    
+    def add_dirs_2(self):
+        dirs = list()
+        for i in range(SP.num_fish):
+            dirs.append(dpg.draw_arrow(
+                p1=[0, 0],
                 p2=[0, 0],
                 thickness=self.pos2pixels(dpg.get_value("fish_radius")),
-                color=[0, 255, 0, 255],
+                color=ColorPalette.fish_alt,
+                parent="Canvas",
+            ))
+        return dirs
+    
+    def add_tails(self):
+        dirs = list()
+        for i in range(SP.num_fish):
+            dirs.append(dpg.draw_arrow(
+                p1=[0, 0],
+                p2=[0, 0],
+                thickness=self.pos2pixels(dpg.get_value("fish_radius")),
+                color=ColorPalette.fish,
                 parent="Canvas",
             ))
         return dirs
